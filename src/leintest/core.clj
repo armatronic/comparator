9d1d900c069b3b8efcef81faed4b420031f8ef64
Amri Sannang <amri.sannang@gmail.com>
2014-04-16 23:00:10 +1000

    Moved core file to use comparator library

--- src/leintest/core.clj
+++ src/leintest/core.clj
@@ -1,29 +1,8 @@
 ;; Sets namespace. What does gen-class do?
 (ns leintest.core
-  (:gen-class))
-
-(require '(clojure [string :as s]))
-(require 'clojure.pprint)
-(use '[clojure.java.io :only (reader)])
-
-(defn rand2-nth
-  "Returns 2 different random values out of the list."
-  [input-sequence]
-  (when-not (coll? input-sequence)
-    (throw (IllegalArgumentException. "expected a collection")))
-  (when (< (count input-sequence) 2)
-    (throw (IllegalArgumentException. "expected a collection with at least two items")))
-  (if (> (count input-sequence) 2)
-    (let [rand1 (rand-nth input-sequence),
-          rand2 (rand-nth input-sequence)]
-      ; Random values same: try again, otherwise return
-      (if (= rand1 rand2)
-        (recur input-sequence)
-        [rand1 rand2]
-        )
-      )
-    [(first input-sequence) (second input-sequence)]
-    )
+  (:gen-class)
+  (:require [leintest.comparator :as c])
+  (:use [clojure.java.io :only (reader)])
   )
 
 (defn valid-selection?
@@ -63,13 +42,11 @@
   [& args]
   ;; work around dangerous default behaviour in Clojure
   (alter-var-root #'*read-eval* (constantly false))
-
   ; Offer me a selection of two DIFFERENT random selections from this file.
   (with-open [rdr (reader "./.gitignore")]
-    (let [item-seq (line-seq rdr),
-          rands    (rand2-nth item-seq),
-          output   (map-indexed (fn [i line] [(+ 1 i) line]) rands)]
-      ; Output the line
+    (let [comparator (c/new-comparator (line-seq rdr))
+          output     (map-indexed (fn [i line] [(+ 1 i) line]) (c/get-unchosen-pair comparator))]
+      ; Output the line (intersperse with map)
       (doseq [line output]
         (println (str (first line) ": " (second line)))
         )
